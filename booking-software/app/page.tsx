345d84aeef74607f8401fb47d4cc5d88d129549b
Humaira Afrin <humaira.afrin.stockholm@gmail.com>
2024-11-13 00:41:03 +0100

    Add booking-software to the repository

---
+++ booking-software/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Calender from "./Components/calender";
+import '../app/Components/calender.css';
+
+export default function Home() {
+  return (
+   <main>
+    <Calender />
+   </main>
+  );
+}
